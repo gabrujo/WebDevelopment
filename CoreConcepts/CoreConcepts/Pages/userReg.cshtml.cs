02c2dfe13fe9ec34c988ee1a61b64a4429b9a806
Gabriel <gabrielaraujofe@gmail.com>
2021-04-18 11:14:32 -0400

    new razor page and others added

---
+++ CoreConcepts/CoreConcepts/Pages/userReg.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoreConcepts.Pages
+{
+    public class userRegModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
