27327c80898c870e10ee539fa99da041cc14fde9
Gabriel <gabrielaraujofe@gmail.com>
2021-04-25 21:22:33 -0400

    MyPartials

---
+++ CoreConcepts/CoreConcepts/Pages/Models/CustomerInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreConcepts.Pages.Models
+{
+    public class CustomerInfo
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public List<StockInfo> STList { get; set; }
+    }
+}
---
+++ CoreConcepts/CoreConcepts/Pages/Models/StockInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreConcepts.Pages.Models
+{
+    public class StockInfo
+    {
+        public string Symbol { get; set; }
+        public double Price { get; set; }
+    }
+
+}
