4b1fcd027054ca10de7b7b50a13c447d6c8b6bed
Gabriel <gabrielaraujofe@gmail.com>
2021-04-30 10:49:42 -0400

    new BankRPSQT

---
+++ BankRPSQL/BankRPSQL/ConnectionStringHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankRPSQL
+{
+    public class ConnectionStringHelper
+    {
+        public static string CONNSTR = "";
+    }
+}
