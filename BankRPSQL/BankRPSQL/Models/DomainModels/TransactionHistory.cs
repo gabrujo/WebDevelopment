b98ca8d3afbf9e28595e01f1cf2aea11194053b2
Gabriel <gabrielaraujofe@gmail.com>
2021-05-01 10:05:33 -0400

    DBList and new folders

---
+++ BankRPSQL/BankRPSQL/Models/DomainModels/TransactionHistory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankRPSQL.Models.DomainModels
+{
+    public partial class TransactionHistory : EntityBase
+    {
+        public long TransactionId { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public long CheckingAccountNumber { get; set; }
+        public long SavingAccountNumber { get; set; }
+        public decimal Amount { get; set; }
+        public decimal TransactionFee { get; set; }
+        public int TransactionTypeId { get; set; }
+    }
+}
---
+++ BankRPSQL/BankRPSQL/Models/ViewModels/TransactionHistoryVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankRPSQL.Models.ViewModels
+{
+    public class TransactionHistoryVM
+    {
+        public string TransactionTypeName
+        {
+            get;
+            set;
+        }
+    }
+}
