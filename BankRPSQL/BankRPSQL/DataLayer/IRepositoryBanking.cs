12e40ee8487021c7bc08fbc294de975d583d8465
Gabriel <gabrielaraujofe@gmail.com>
2021-05-01 10:57:00 -0400

    service/business layer

---
+++ BankRPSQL/BankRPSQL/DataLayer/IRepositoryBanking.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BankRPSQL.Models.ViewModels;
+
+namespace BankRPSQL.DataLayer
+{
+    public interface IRepositoryBanking
+    {
+        decimal GetCheckingBalance(long checkingAccountNum);
+        decimal GetSavingBalance(long savingAccountNum);
+        long GetCheckingAccountNumForUser(string username);
+        long GetSavingAccountNumForUser(string username);
+        bool TransferCheckingToSaving(long checkingAccountNum, long savingAccountNum,
+       decimal amount, decimal transactionFee);
+        bool TransferSavingToChecking(long checkingAccountNum, long savingAccountNum,
+       decimal amount, decimal transactionFee);
+        List<TransactionHistoryVM> GetTransactionHistory(long checkingAccountNum);
+    }
+
+}
