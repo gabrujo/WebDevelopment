f8de638973b56080185279113f427bc14ee5d963
Gabriel <gabrielaraujofe@gmail.com>
2021-05-03 19:58:05 -0400

    session facade and others

---
+++ BankRPEF/BankRPEF/Utils/SessionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace BankRPEF.Utils
+{
+    public static class SessionExtensions // all methods are static
+    {
+        // we will extend the ISession interface to provide
+        // generic Set<T> and Get<T> type safe access to Session
+        public static void Set<T>(this ISession session, string key, T val)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(val));
+        }
+        public static T Get<T>(this ISession session, string key)
+        {
+            var objstr = session.GetString(key);
+            if (objstr == null)
+                return default(T);
+            else
+                return JsonConvert.DeserializeObject<T>(objstr);
+        }
+    }
+}
