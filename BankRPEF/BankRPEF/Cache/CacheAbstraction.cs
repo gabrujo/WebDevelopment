a6a54fc20cc45d8362c65ffc4a18f7d0cb64ec5d
Gabriel <gabrielaraujofe@gmail.com>
2021-05-03 20:50:45 -0400

    cache abstraction added

---
+++ BankRPEF/BankRPEF/Cache/CacheAbstraction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BankRPEF.Cache.BankRPEF.Cache;
+
+namespace BankRPEF.Cache
+{
+    public class CacheAbstraction
+    {
+        IWebCache _iwebCache = null;
+        public CacheAbstraction(IWebCache iwebc)
+        {
+            _iwebCache = iwebc;
+        }
+        public CacheAbstraction() // default cache provider
+                                  // : this(new HttpContextCacheAdapter())
+        : this(new CouchBaseCacheAdapter())
+        //: this(new NullObjectCache())
+        {
+        }
+        public void Remove(string key)
+        {
+            _iwebCache.Remove(key);
+        }
+        public void Insert(string key, object obj)
+        {
+            _iwebCache.Store(key, obj);
+        }
+        public T Retrieve<T>(string key)
+        {
+            return _iwebCache.Retrieve<T>(key);
+        }
+    }
+
+}
+}
---
+++ BankRPEF/BankRPEF/Utils/CacheAbstractionHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankRPEF.Cache
+{
+    public class CacheAbstractionHelper
+    {
+        public static CacheAbstraction CABS = null;
+    }
+
+}
