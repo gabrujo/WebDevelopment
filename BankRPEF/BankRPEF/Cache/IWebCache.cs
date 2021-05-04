cf25a1a1dfb9af5cfd58f214394587563550fec6
Gabriel <gabrielaraujofe@gmail.com>
2021-05-03 20:37:39 -0400

    web cache

---
+++ BankRPEF/BankRPEF/Cache/IWebCache.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankRPEF.Cache
+{
+    public interface IWebCache
+    {
+        // A cache provider needs to implement this interface
+        // via an adapter
+        void Remove(string key);
+        void Store(string key, object obj);
+        T Retrieve<T>(string key);
+    }
+}
