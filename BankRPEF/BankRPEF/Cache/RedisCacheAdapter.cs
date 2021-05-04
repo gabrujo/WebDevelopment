39e104d4a9a8d56a4e3cff8382e0e77a8c0efd4b
Gabriel <gabrielaraujofe@gmail.com>
2021-05-03 21:19:26 -0400

    changes in rediscacheadapter

---
+++ BankRPEF/BankRPEF/Cache/RedisCacheAdapter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BankRPEF.Utils;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace BankRPEF.Cache
+{
+    public class RedisCacheAdapter : IWebCache
+    {
+        public void Remove(string key)
+        {
+            RedisCacheHelper.IDISTCACHE.Remove(key);
+        }
+        public T Retrieve<T>(string key)
+        {
+            string data = RedisCacheHelper.IDISTCACHE.GetString(key);
+            if (data != null)
+                return JsonConvert.DeserializeObject<T>(data);
+            else
+                return default(T);
+        }
+        public void Store(string key, object obj)
+        {
+            string objdata = JsonConvert.SerializeObject(obj);
+            RedisCacheHelper.IDISTCACHE.SetString(key, objdata);
+        }
+    }
+
+}
