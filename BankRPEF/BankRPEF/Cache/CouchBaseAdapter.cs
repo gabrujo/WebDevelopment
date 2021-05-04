b8a986606bc9f18099367bb34c979e97685ac63a
Gabriel <gabrielaraujofe@gmail.com>
2021-05-03 20:47:21 -0400

    coch base adapter

---
+++ BankRPEF/BankRPEF/Cache/CouchBaseAdapter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankRPEF.Cache
+{
+    using Couchbase;
+    using Couchbase.Configuration.Client;
+    using Couchbase.Configuration.Server.Providers;
+    using Couchbase.Core;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    namespace BankRPEF.Cache
+    {
+        public class CouchBaseCacheAdapter : IWebCache
+        {
+            static ClientConfiguration config = null;
+            private readonly Cluster cluster = null;
+            IBucket _ibucket = null;
+            public CouchBaseCacheAdapter()
+            {
+                config = new ClientConfiguration
+                {
+                    Servers = new List<Uri>
+ {
+ //new Uri("http://127.0.0.1:8091/pools"),
+new Uri("http://127.0.0.1:8091")
+ }
+                };
+                //config.ConfigurationProviders = ServerConfigurationProviders.HttpStreaming;
+                cluster = new Cluster(config);
+                cluster.Authenticate("Administrator", "couchbase");//from your couchbase
+                _ibucket = cluster.OpenBucket("mybucket");
+            }
+            // for CouchBase
+            #region IWebCache Members
+            public void Remove(string key)
+            {
+                try
+                {
+                    var upsert = _ibucket.Remove(key);
+                }
+                catch (Exception ex)
+                {
+                    throw (ex);
+                }
+            }
+            public void Store(string key, object obj)
+            {
+                try
+                {
+                    var upsert = _ibucket.Upsert(key, obj);
+                }
+                catch (Exception ex)
+                {
+                    throw (ex);
+                }
+            }
+            public T Retrieve<T>(string key)
+            {
+                T cachedData = default(T);
+                var data = _ibucket.GetDocument<T>(key);
+                cachedData = data.Document.Content;
+                return cachedData;
+            }
+            #endregion
+        }
+    }
+
+}
---
+++ BankRPEF/BankRPEF/Cache/NullCacheAdapter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankRPEF.Cache
+{
+    public class NullObjectCache : IWebCache
+    { // so that no caching can be easily implemented
+        public void Remove(string key)
+        {
+        }
+        public void Store(string key, object obj)
+        {
+        }
+        public T Retrieve<T>(string key)
+        {
+            return default(T);
+        }
+    }
+}
