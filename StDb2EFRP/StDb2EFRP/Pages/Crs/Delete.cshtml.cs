eeff9864de62c38915082da159a36a4fad0dadb2
Gabriel <gabrielaraujofe@gmail.com>
2021-05-05 17:41:19 -0400

    a razor page to it using the Entity

---
+++ StDb2EFRP/StDb2EFRP/Pages/Crs/Delete.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using StDb2EFRP;
+
+namespace StDb2EFRP.Pages.Crs
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly StDb2EFRP.StDb2SqlContext _context;
+
+        public DeleteModel(StDb2EFRP.StDb2SqlContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Course Course { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Course = await _context.Courses.FirstOrDefaultAsync(m => m.CourseNum == id);
+
+            if (Course == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Course = await _context.Courses.FindAsync(id);
+
+            if (Course != null)
+            {
+                _context.Courses.Remove(Course);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ StDb2EFRP/StDb2EFRP/Pages/Crs/Details.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using StDb2EFRP;
+
+namespace StDb2EFRP.Pages.Crs
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly StDb2EFRP.StDb2SqlContext _context;
+
+        public DetailsModel(StDb2EFRP.StDb2SqlContext context)
+        {
+            _context = context;
+        }
+
+        public Course Course { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Course = await _context.Courses.FirstOrDefaultAsync(m => m.CourseNum == id);
+
+            if (Course == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
