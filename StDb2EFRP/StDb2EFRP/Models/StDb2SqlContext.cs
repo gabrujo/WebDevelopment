434323c35f8c04847b115ebfbdfd56a4129b8be0
Gabriel <gabrielaraujofe@gmail.com>
2021-05-05 16:59:50 -0400

    changes in server configuration

--- StDb2EFRP/StDb2EFRP/Models/StDb2SqlContext.cs
+++ StDb2EFRP/StDb2EFRP/Models/StDb2SqlContext.cs
@@ -27,14 +27,14 @@ namespace StDb2EFRP
         public virtual DbSet<VwCoursestaken> VwCoursestakens { get; set; }
         public virtual DbSet<VwGrade> VwGrades { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+      /*  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("server=DESKTOP-4US0OAN\\SQLEXPRESS;Trusted_Connection=True;MultipleActiveResultSets=true;database=StDb2Sql;");
             }
-        }
+        } */
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
