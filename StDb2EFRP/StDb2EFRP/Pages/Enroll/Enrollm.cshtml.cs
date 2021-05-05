77d106d546e4cdb87246b0022cf99dc6baf430f8
Gabriel <gabrielaraujofe@gmail.com>
2021-05-05 17:46:14 -0400

    entity framework page added

---
+++ StDb2EFRP/StDb2EFRP/Pages/Enroll/Enrollm.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using StDb2EFRP;
+
+namespace StDb2EFRP.Pages.Enroll
+{
+    public class EnrollmModel : PageModel
+    {
+        private readonly StDb2EFRP.StDb2SqlContext _context;
+
+        public EnrollmModel(StDb2EFRP.StDb2SqlContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Enrollment> Enrollment { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Enrollment = await _context.Enrollments
+                .Include(e => e.CourseNumNavigation)
+                .Include(e => e.Student).ToListAsync();
+        }
+    }
+}
