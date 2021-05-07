e7f0169bf2e40abc7fb98d8f6d5721ccfa988918
Gabriel <gabrielaraujofe@gmail.com>
2021-05-06 23:59:41 -0400

    interface and class added

---
+++ AjaxCore/AjaxCore/Services/ICourseService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AjaxCore.Models;
+
+namespace AjaxCore.Services
+{
+    public interface ICourseService
+    {
+        List<Semester> GetSemesters();
+        List<Course> GetCoursesForASemester(int semesterId);
+        List<Enrollment> GetEnrollmentForACourse(string courseNum);
+    }
+}
