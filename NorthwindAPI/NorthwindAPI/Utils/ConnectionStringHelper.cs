5c2dd042d3805e77228a52acc9fdfe06d3492ba2
Gabriel <gabrielaraujofe@gmail.com>
2021-05-07 15:30:17 -0400

    connectionstring and utils folder

---
+++ NorthwindAPI/NorthwindAPI/Utils/ConnectionStringHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NorthwindAPI.Utils
+{
+    public class ConnectionStringHelper
+    {
+        public static string CONNSTR = "";
+    }
+
+}
