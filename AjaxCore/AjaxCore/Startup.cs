67bee32aaaafd31f9759f9479fe3dcab5afc87f6
Gabriel <gabrielaraujofe@gmail.com>
2021-05-07 00:08:39 -0400

    changes in startup.cs and layout

--- AjaxCore/AjaxCore/Startup.cs
+++ AjaxCore/AjaxCore/Startup.cs
@@ -12,6 +12,7 @@ using AjaxCore.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using AjaxCore.Services;
 
 namespace AjaxCore
 {
