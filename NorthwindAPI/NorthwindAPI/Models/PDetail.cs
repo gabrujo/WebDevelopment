c1307b5e40eae2acbb38cf3c19a617a8c9b5f433
Gabriel <gabrielaraujofe@gmail.com>
2021-05-07 15:25:04 -0400

    new classes and appsettings.json change

---
+++ NorthwindAPI/NorthwindAPI/Models/PDetail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NorthwindAPI.Models
+{
+    public class PDetail
+    {
+        public int UnitsInStock { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public bool Discontinued { get; set; }
+    }
+}
---
+++ NorthwindAPI/NorthwindAPI/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NorthwindAPI
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string SupplierName { get; set; }
+        public string CategoryName { get; set; }
+        public decimal Price { get; set; }
+    }
+}
