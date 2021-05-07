d2934fa8cd3d438b44754cacd44e2f868c6170f8
Gabriel <gabrielaraujofe@gmail.com>
2021-05-07 00:05:43 -0400

    Add Customer

--- AjaxCore/AjaxCore/Startup.cs
+++ AjaxCore/AjaxCore/Startup.cs
@@ -33,6 +33,7 @@ namespace AjaxCore
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
+            services.AddSingleton<ICourseService, CourseService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
